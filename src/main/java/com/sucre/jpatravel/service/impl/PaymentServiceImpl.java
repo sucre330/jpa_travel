4fa8d9f11402563890c315b435136c433814f153
sucre <330675022@qq.com>
2021-08-08 22:54:38 +0800

    use caffeine by second way

--- src/main/java/com/sucre/jpatravel/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/sucre/jpatravel/service/impl/PaymentServiceImpl.java
@@ -1,24 +1,30 @@
 package com.sucre.jpatravel.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
 import com.sucre.jpatravel.dao.PaymentRepository;
 import com.sucre.jpatravel.model.Payment;
 import com.sucre.jpatravel.service.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import cn.hutool.core.date.DateUtil;
 
 @Service
+@CacheConfig(cacheNames = "caffeineCacheManager")
 public class PaymentServiceImpl implements PaymentService {
 
     @Autowired
     private PaymentRepository paymentRepository;
 
     @Override
+    @Cacheable(key = "#id")
     public Payment getPaymentInfo() {
-//        return paymentRepository.findOneById(1);
-      List<Payment> list=  paymentRepository.findAll();
-      return null;
+    	String id="1";
+    	System.out.println("coming..."+ DateUtil.date());
+        Payment payment =  paymentRepository.findAll().stream().findFirst().get();
+      return payment;
 
     }
 }
